3a6c3f81955a89dc652ab946b02e25a41ace0373
navigaid <navigaid@gmail.com>
2024-02-20 15:46:48 +0000

    add v0 component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
 import Image from "next/image";
+import { Component } from "@/components/component"
 
 export default function Home() {
+	return (
+		<Component />
+	)
+}
+
+export function OldHome() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
